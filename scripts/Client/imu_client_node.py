df711ed362c9673708f8413166470d6560173552
jacorderopr <jacorderopr@gmail.com>
2022-03-27 13:28:22 -0400

    added the documented version of imu client node

--- scripts/Client/imu_client_node.py
+++ scripts/Client/imu_client_node.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
-# git kraken git pull request, git lenght
+
+"""Recommended Order to read and understand the IMU task:
+
+1. imu_data.py 
+2. imu_connetory.py 
+3. server.py 
+4. imu_client_node.py (YOU ARE HERE!) 
+
+"""
+
 
 
 import rospy
@@ -11,11 +20,21 @@ from geometry_msgs.msg import Vector3, Quaternion
 
 from tf.transformations import euler_from_quaternion
 
+# ignore the vectors and letters list (they are commented out)
 # WE NEED TO DO THE EULER CALCULATIONS ONCE WE GET HERE!!!! CALCULATIONS CANNOT BE DONE IN PYTHON3!!
-vectors = ["ypr", "attitude", "imu_rate", "angular_rate", "acceleration", "imu_acceleration", "mag", "dtheta", "dvel" ]
-letters = ["x", "y", "z", "w"]
+# vectors = ["ypr", "attitude", "imu_rate", "angular_rate", "acceleration", "imu_acceleration", "mag", "dtheta", "dvel" ]
+# letters = ["x", "y", "z", "w"]
 
 class IMU_Node(object):
+    """Help on the IMU_Node Class:
+    
+    -> creating a class varaible named BASE that holds the BASE route of needed to access the Flask server and execute the GET request (see server.py for more information)
+    
+    __init__ method:
+    
+    -> Initialization of ROS node followed by creation of ROS publisher passing as arguments the following (topic_name, type_of_msg, queue size)
+    
+    -> Next, we create an instance of the Raw_IMU() object to refer to said instance and set its values to the values being received from the flask server. """
     BASE = "http://127.0.0.1:5000//imu"
     def __init__(self):
         rospy.init_node("imu_node", anonymous=True)
@@ -25,20 +44,54 @@ class IMU_Node(object):
         self.rate = rospy.Rate(10)
 
     def get_data(self):
+        """Help on the get_data method:
+        
+        -> Utilizing the 'get' function that was imported from the requests module (see import statements on top) we are able to retrieve the server
+        data by passing in the base route being: http://127.0.0.1:5000//imu (connects to server and executes the get method in which the server will
+        return the show_data method of the Test class)
+        
+        ->SERVER_RESPONSE VARIABLE: Notice how we perform the get function FIRST and then turn what was received from the get method to a JSON. This is done since we want to turn 
+        the requested data to a dictionary (using .json()) to be able to access the server response.
+        
+        -> After creating the server_response variable which returns a dictionary that contains all of the attributes as keys and their respective values, we can 
+        begin to set the IMU_MSG object's attributes (which is the ROS MESSAGE) to the respective key of the server_reponse dictionary
+        
+            example:
+            
+            self.imu_msg.ypr.x = server_response["ypr_x"] 
+            
+            --> Here the left side represents the attribute of the IMU_MSG that we want to set. We set said attribute to the VALUE of the KEY named 'ypr_x'. 
+            Remember that this dictionary is an exact copy of the vars(self) of the Test class (or IMU_DATA class) This copycomes from the server which in 
+            turn comes from the show_data method inside the Test class.  """
+        
         # print(dir(self.imu_msg))
         server_response = get(self.BASE).json() # CRUCIAL: we turn it into a json BEFORE accessing its items. (not a dict before json())
         
+        print("The server response is: ", server_response)
         # # # now that server_response is a dictionary then we can see the attributes and its values by using server_response[ATTRIBUTE]
+        # vectors = ["ypr", "attitude", "imu_rate", "angular_rate", "acceleration", "imu_acceleration", "mag", "dtheta", "dvel" ]
+        # letters = ["x", "y", "z", "w"]
         
         # for attri in vectors:
-        
-        #     if attri == "attitude":
-        #         for letter in letters: # iterate over the complete list
-        #             setattr(self.imu_msg, attri+".{}".format(letter), server_response[attri+"_{}".format(letter)])
-        #     elif attri in dir(self.imu_msg):
-        #         for letter in letters[:3]:
-        #             setattr(self.imu_msg, attri+".{}".format(letter), server_response(attri+"_{}".format(letter)))
+        #     for letter in letters: # iterate over the complete list
+        #             if (letter == "w" and attri != "attitude"): # since only attitude is a quaternion, then attitude is the only one allowed to use 'w'.
+        #                 continue
                     
+        #             else:
+        #                 display_imu_msg_attri = attri+"."+letter # THIS VAR is for debugging purposes only. (to be printed on line 39)
+        #                 display_server_attri = attri+"_"+letter
+        #                 print("DEBUGGING PRINT MESSAGE: Setting {} imu_msg attribute to the value of {}".format(display_imu_msg_attri, display_server_attri))
+
+        #                 setattr(self.imu_msg, attri+".{}".format(letter), server_response[attri+"_{}".format(letter)]) 
+        
+            # elif attri in dir(self.imu_msg):
+            #     for letter in letters[:3]:
+            #         display_imu_msg_attri = attri+"."+letter
+            #         display_server_attri = attri+"_"+letter
+            #         print("DEBUGGING PRINT MESSAGE: Setting {} imu_msg attribute to the value of {}".format(display_imu_msg_attri, display_server_attri))
+            #         setattr(self.imu_msg, attri+".{}".format(letter), server_response[attri+"_{}".format(letter)])
+                    
+            
         self.imu_msg.ypr.x = server_response["ypr_x"]
         self.imu_msg.ypr.y = server_response["ypr_y"]
         self.imu_msg.ypr.z = server_response["ypr_z"]
@@ -49,22 +102,27 @@ class IMU_Node(object):
         self.imu_msg.attitude.z = server_response['attitude_z']
         self.imu_msg.attitude.w = server_response['attitude_w']
 
-        self.attitude = [self.imu_msg.attitude.x, self.imu_msg.attitude.y, self.imu_msg.attitude.z, self.imu_msg.attitude.w]
+        self.attitude = [self.imu_msg.attitude.x, self.imu_msg.attitude.y, self.imu_msg.attitude.z, self.imu_msg.attitude.w] # this self.attitude
+        # attribute creates a list and references that same list ALWAYS!! if you change the values of self.imu_msg.attitude.x, the list will NOT update.
 
-        self.orientation_x, self.orientation_y, self.orientation_z, self.orientation_w = list(euler_from_quaternion(self.attitude))
+        self.imu_msg.orientation.x, self.imu_msg.orientation.y, self.imu_msg.orientation.z = list(euler_from_quaternion(self.attitude))
 
 
-        self.imu_msg.accelaration.x = server_response['accelaration_x']
-        self.imu_msg.accelaration.y = server_response['accelaration_y']
-        self.imu_msg.accelaration.z = server_response['accelaration_z']
+        self.imu_msg.acceleration.x = server_response['acceleration_x']
+        self.imu_msg.acceleration.y = server_response['acceleration_y']
+        self.imu_msg.acceleration.z = server_response['acceleration_z']
+
+        self.imu_msg.angular_rate.x = server_response['angular_rate_x']
+        self.imu_msg.angular_rate.y = server_response['angular_rate_y'] # added this part... testing.
+        self.imu_msg.angular_rate.z = server_response['angular_rate_z']
 
         self.imu_msg.imu_rate.x = server_response['imu_rate_x']
         self.imu_msg.imu_rate.y = server_response['imu_rate_y']
         self.imu_msg.imu_rate.z = server_response['imu_rate_z']
 
-        self.imu_msg.imu_accelaration.x = server_response['imu_acceleration_x']
-        self.imu_msg.imu_accelaration.y = server_response['imu_acceleration_y']
-        self.imu_msg.imu_accelaration.z = server_response['imu_acceleration_z']
+        self.imu_msg.imu_acceleration.x = server_response['imu_acceleration_x']
+        self.imu_msg.imu_acceleration.y = server_response['imu_acceleration_y']
+        self.imu_msg.imu_acceleration.z = server_response['imu_acceleration_z']
 
         self.imu_msg.mag.x = server_response['mag_x']
         self.imu_msg.mag.y = server_response['mag_y']
@@ -84,7 +142,7 @@ class IMU_Node(object):
         
         
         
-        
+      
         # for key , val in server_response.items(): # when we create it into a json() then it 'turns' from a response object to a dictionary
         #     if key in dir(self.imu_msg):
         #         setattr(self.imu_msg, key, val) # set the attribute of the object to the the value of the key.
@@ -92,7 +150,7 @@ class IMU_Node(object):
         # # print("Dir function: {}".format(dir(self.imu_msg)))# the dir function returns all of the methods and attributes of 
         # return self.publish_data(self.imu_msg) # so we have to make it a json() to not get 'response(200)
     
-    def publish_data(self, server_data):
+    def publish_data(self): # I removed a parameter called server_data of this function.
         self._pub.publish(self.imu_msg)
         return self.imu_msg
 
@@ -104,6 +162,8 @@ if __name__ == "__main__":
         # print(imu_node.get_data())
         while not(rospy.is_shutdown()):
             imu_node.get_data()
+            imu_node.publish_data()
+            imu_node.rate.sleep() # not sure about this line. check later.
             
 
     except ConnectionError:
